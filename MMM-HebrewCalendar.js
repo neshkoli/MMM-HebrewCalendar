4a1ceb26c06a8a39d6d3a72c4b833fbd92507ce8
Noam Eshkoli <neshkoli@gmail.com>
2023-11-13 23:01:13 +0200

    Create project

---
+++ MMM-HebrewCalendar.js
@@ -0,0 +1,274 @@
+// MMM-HebrewCalendar.js
+
+
+function el(tag, options) {
+	var result = document.createElement(tag);
+	options = options || {};
+	for (var key in options) {
+		result[key] = options[key];
+	}
+	return result;
+}
+
+function addOneDay(d) {
+	return new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1, d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds());
+}
+
+function diffDays(a, b) {
+	a = new Date(a);
+	b = new Date(b);
+
+	a.setHours(0,0,0,0);
+	b.setHours(0,0,0,0);
+
+	return Math.round((a.getTime() - b.getTime()) / (24 * 60 * 60 * 1000)) + 1;
+}
+
+function getHebYear(date) {
+	return new Intl.DateTimeFormat("en-u-ca-hebrew", { year: "numeric" }).format(date);
+}
+
+function getHebMonth(date) {
+	return new Intl.DateTimeFormat("he-u-ca-hebrew", { month: "numeric" }).format(date);
+}
+
+function getHebDay(date) {
+	const hday = new Intl.DateTimeFormat("en-u-ca-hebrew", { day: "numeric" }).format(date);
+	return hday;
+}
+
+function equals(a, b) {
+	if (typeof a !== typeof b) {
+		return false;
+	}
+
+	if (!!a && (a.constructor === Array || a.constructor === Object)) {
+		for (var key in a) {
+			if (!b.hasOwnProperty(key) || !equals(a[key], b[key])) {
+				return false;
+			}
+		}
+		return true;
+	} else if (!!a && a.constructor == Date) {
+		return a.valueOf() === b.valueOf();
+	}
+	return a === b;
+}
+
+function getLuminance(color) {
+	try {
+		const [r, g, b, a, s, d] = color.match(/([0-9.]+)/g);
+		return 0.299 * +r + 0.587 * +g + 0.114 * +b;
+	} catch {
+		return 0;
+	}
+}
+
+Module.register("MMM-HebrewCalendar", {
+	// Default module config
+	defaults: {
+		mode: "FourWeeks",
+		firstDayOfWeek: "sunday",
+		displaySymbol: false,
+		wrapTitles: true,
+		hideCalendars: []
+	},
+
+	start: function () {
+		var self = this;
+
+		self.sourceEvents = {};
+		self.events = [];
+		self.displayedDay = null;
+		self.displayedEvents = [];
+		self.updateTimer = null;
+		self.skippedUpdateCount = 0;
+	},
+
+	notificationReceived: function (notification, payload, sender) {
+		var self = this;
+
+		if (notification === "CALENDAR_EVENTS") {
+			self.sourceEvents[sender.identifier] = payload
+				.map((e) => {
+					e.startDate = new Date(+e.startDate);
+					e.endDate = new Date(+e.endDate);
+
+					if (e.fullDayEvent) {
+						e.endDate = new Date(e.endDate.getTime() - 1000);
+						if (e.startDate > e.endDate) {
+							e.startDate = new Date(e.endDate.getFullYear(), e.endDate.getMonth(), e.endDate.getDate(), 1);
+						} else {
+							e.startDate = new Date(e.startDate.getTime() + 60 * 60 * 1000);
+						}
+					}
+					return e;
+				})
+				.filter((e) => {
+					return !self.config.hideCalendars.includes(e.calendarName);
+				});
+
+			if (self.updateTimer !== null) {
+				clearTimeout(self.updateTimer);
+				++self.skippedUpdateCount;
+			}
+
+			self.updateTimer = setTimeout(() => {
+				var today = new Date().setHours(12, 0, 0, 0).valueOf();
+
+				self.events = Object.values(self.sourceEvents)
+					.reduce((acc, cur) => acc.concat(cur), [])
+					.sort((a, b) => { return a.startDate - b.startDate; });
+				if (
+					today !== self.displayedDay ||
+					!equals(self.events, self.displayedEvents)
+				) {
+					self.displayedDay = today;
+					self.displayedEvents = self.events;
+					self.updateTimer = null;
+					self.skippedUpdateCount = 0;
+					self.updateDom();
+				}
+			}, 5000);
+		}
+	},
+
+	getStyles: function () {
+		return ["MMM-HebrewCalendar.css"];
+	},
+
+	getDom: function () {
+		const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		const hebDayArray = [
+			"", "א׳", "ב׳", "ג׳", "ד׳", "ה׳", "ו׳", "ֿז׳", "ח׳", "ט׳", "י׳", 'י"א', 'י"ב', 'י"ג', 'י"ד', 'ט"ו', 'ט"ז',
+			'י"ז', 'י"ח', 'י"ט', "כ׳", 'כ"א', 'כ"ב', 'כ"ג', 'כ"ד', 'כ"ה', 'כ"ו', 'כ"ז', 'כ"ח', 'כ"ט', "ל׳"];
+		const weeksToMonthDays = { nextoneweek: 0, currentweek: 0, oneweek: 0, twoweeks: 7, threeweeks: 14, fourweeks: 21, nextfourweeks: 21 };
+		const self = this;
+		const now = new Date();
+		const table = el("table", { className: "small wrapper" });
+		const today = now.getDate();
+		const mode = self.config.mode.toLowerCase();
+		let firstDayOfWeek = self.config.firstDayOfWeek.toLowerCase();
+		var row = el("tr");
+		var cell;
+		var cellIndex, monthDays;
+		var dateCells = [];
+		var startDayOffset = 0;
+
+		if (firstDayOfWeek === "today") {
+			firstDayOfWeek = days[now.getDay()].toLowerCase();
+		}
+
+		while (firstDayOfWeek !== days[0].toLowerCase() && startDayOffset < days.length) {
+			days.push(days.shift());
+			++startDayOffset;
+		}
+
+		startDayOffset = startDayOffset % 7;
+
+		if (mode in weeksToMonthDays) {
+			cellIndex = today - now.getDay() + startDayOffset;
+			while (cellIndex > today) {
+				cellIndex -= 7;
+			}
+			monthDays = cellIndex + weeksToMonthDays[mode];
+		} else {
+			if (mode === "lastmonth") {
+				now.setMonth(now.getMonth() - 1);
+			} else if (mode === "nextmonth") {
+				now.setMonth(now.getMonth() + 1);
+			}
+			cellIndex = 1 - new Date(now.getFullYear(), now.getMonth(), 1).getDay() + startDayOffset;
+			monthDays = 32 - new Date(now.getFullYear(), now.getMonth(), 32).getDate();
+			while (cellIndex > 1) {
+				cellIndex -= 7;
+			}
+		}
+
+		for (var day = 0; day < 7; ++day) {
+			const headerDate = new Date(now.getFullYear(), now.getMonth(), cellIndex + day);
+			row.appendChild(el("th", { className: "header", innerHTML: headerDate.toLocaleString(config.language, { weekday: "long" }) }));
+		}
+		table.appendChild(row);
+
+		for (var week = 0; week < 6 && cellIndex <= monthDays; ++week) {
+			row = el("tr", { className: "small" });
+
+			for (day = 0; day < 7; ++day, ++cellIndex) {
+				var cellDate = new Date(now.getFullYear(), now.getMonth(), cellIndex);
+				var hebDay = getHebDay(cellDate);
+				var hebMonth = getHebMonth(cellDate);
+				var cellDay = cellDate.getDate();
+
+				cell = el("td", { className: "cell" });
+				if (["lastmonth", "nextmonth"].includes(mode)) {
+					// Do nothing
+				} else if (day === 6) {
+					cell.classList.add("shabbat");
+				} else if (cellIndex === today) {
+					cell.classList.add("today");
+				} else if (cellIndex !== cellDay && mode === "currentmonth") {
+					cell.classList.add("other-month");
+				} else if (cellIndex < today) {
+					cell.classList.add("past-date");
+				}
+
+				if ((week === 0 && day === 0) || cellDay === 1) {
+					cellDay = cellDate.toLocaleString(config.language, { month: "short", day: "numeric" });
+				}
+				const cellHeader = el("div", { className: "cell-header" });
+				const hebHeader = parseInt(hebDay) === 1 ? el("div", { className: "heb-header", innerHTML: hebDayArray[hebDay] + " " + hebMonth }) : el("div", { className: "heb-header", innerHTML: hebDayArray[hebDay] });
+				const dateHeader = el("div", { className: "date-header", innerHTML: cellDay });
+				cellHeader.appendChild(hebHeader);
+				cellHeader.appendChild(dateHeader);
+				cell.appendChild(cellHeader);
+				row.appendChild(cell);
+				dateCells[cellIndex] = cell;
+			}
+			table.appendChild(row);
+		}
+
+		var monthStart = new Date(now.getFullYear(), now.getMonth(), 1);
+		for (var i in self.events) {
+			var e = self.events[i];
+			for (
+				var eventDate = e.startDate;
+				eventDate <= e.endDate;
+				eventDate = addOneDay(eventDate)
+			) {
+				var dayDiff = diffDays(eventDate, monthStart);
+				if (dayDiff in dateCells) {
+					let div = el("div", { className: "event" });
+					if (!self.config.wrapTitles) {
+						div.classList.add("event-nowrap");
+					}
+					if (!e.fullDayEvent) {
+						function formatTime(d) {
+							var h = d.getHours();
+							var m = d.getMinutes().toString().padStart(2, "0");
+							if (config.timeFormat === 12) {
+								return ((h % 12 || 12) + (m > 0 ? `:${m}` : "") + (h < 12 ? "am" : "pm"));
+							} else {
+								return `${h}:${m}`;
+							}
+						}
+						var eventLine = el("div", { className: "event-line" });
+						eventLine.appendChild(el("div", { className: "event-label", innerHTML: formatTime(e.startDate) }));
+						eventLine.appendChild(el("div", { className: "event-text", innerHTML: e.title }));
+						div.appendChild(eventLine);
+					} else {
+						div.appendChild(el("div", { className: "event-full-day", innerHTML: e.title }));
+					}
+
+					if (self.config.displaySymbol) {
+						for (let symbol of e.symbol) {
+							div.appendChild(el("div", { className: `fa fa-${symbol}` }));
+						}	
+					}
+					dateCells[dayDiff].appendChild(div);
+				}
+			}
+		}
+		return table;
+	}
+});
---
+++ README.md
@@ -0,0 +1,49 @@
+# Module: MMM-HebrewCalendar
+The module allows you to view Hebrew calendar with your events in a monthly calendar view.
+Based on the work of kolbyjack on MMM-MonthlyCalendar
+![Screenshot of module in use.](./screenshot.png)
+
+## Installation
+
+In your terminal, go to your MagicMirror's Module folder:
+````
+cd ~/MagicMirror/modules
+````
+
+Clone this repository:
+````
+git clone https://github.com/kolbyjack/MMM-HebrewCalendar.git
+````
+
+Configure the module in your `config.js` file.
+
+**Note:** After starting the Mirror, it will take a few seconds before events start to appear.
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+````javascript
+modules: [
+  {
+    module: "MMM-HebrewCalendar",
+    position: "bottom_bar",
+    config: { // See "Configuration options" for more information.
+      mode: "fourWeeks",
+    }
+  }
+]
+````
+
+You will likely also want to set `"broadcastPastEvents": true` in your [calendar module configuration](https://docs.magicmirror.builders/modules/calendar.html#configuration-options) so past events are still displayed.
+
+## Configuration options
+
+The following properties can be configured:
+
+|Option|Default|Description|
+|---|---|---|
+|`mode`|`"currentMonth"`|Which type of calendar to show.  Possible values are `lastMonth`, `currentMonth`, `nextMonth`, `currentWeek`, `twoWeeks`, `threeWeeks`, and `fourWeeks`.|
+|`displaySymbol`|`false`|Whether to show symbols next to events.|
+|`firstDayOfWeek`|`"sunday"`|Which day to use as the start of the week.  Use `"today"` to always show the current day in the first column.|
+|`hideCalendars`|`[]`|A list of calendar names to hide from the view.|
+|`wrapTitles`|`true`|Whether to allow event titles to wrap across multiple lines or truncate them.|
